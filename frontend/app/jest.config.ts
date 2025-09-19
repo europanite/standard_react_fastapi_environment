9737a8025da945a700ec8341a300245140a851b0
europanite <europanite@gmail.com>
2025-09-20 00:11:03 +0900

    fix/testfront

--- frontend/app/jest.config.ts
+++ frontend/app/jest.config.ts
@@ -1,27 +1,29 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'jest-expo',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native'
-      + '|@react-native'
-      + '|react-clone-referenced-element'
-      + '|@react-navigation'
-      + '|react-navigation'
-      + '|expo(nent)?'
-      + '|@expo(nent)?/.*'
-      + '|expo-modules-core'
-      + '|expo-.*'
-      + '|@expo/.*'
-      + '|@react-native/polyfills'
-    + ')/)',
-  ],
+ module.exports = {
+   preset: 'jest-expo',
+   testEnvironment: 'node',
+   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '\\.(png|jpe?g|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    'react-native-reanimated': 'react-native-reanimated/mock',
-  },
+     '\\.(png|jpe?g|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+     'react-native-reanimated': 'react-native-reanimated/mock',
+    '^expo($|/.+)': '<rootDir>/__mocks__/expoMock.js',
+    '^expo-router($|/.+)': '<rootDir>/__mocks__/expoRouterMock.js',
+    '^expo-constants$': '<rootDir>/__mocks__/expoConstantsMock.js',
+   },
   setupFiles: ['<rootDir>/jest.setupFiles.js'],
-  testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
-};
+   transformIgnorePatterns: [
+     'node_modules/(?!(react-native'
+       + '|@react-native'
+       + '|react-clone-referenced-element'
+       + '|@react-navigation'
+       + '|react-navigation'
+       + '|expo(nent)?'
+       + '|@expo(nent)?/.*'
+       + '|expo-modules-core'
+       + '|expo-.*'
+       + '|@expo/.*'
+       + '|@react-native/polyfills'
+     + ')/)',
+   ],
+   testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+ };
--- frontend/app/jest.setupFiles.js
+++ frontend/app/jest.setupFiles.js
@@ -1,14 +1,9 @@
-jest.mock('@react-native/animated', () => ({}), { virtual: true });
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => ({}), { virtual: true });
-
-// --- Disable Expo "winter" import-meta registry in Jest ---
 if (!Object.getOwnPropertyDescriptor(globalThis, '__ExpoImportMetaRegistry')) {
   Object.defineProperty(globalThis, '__ExpoImportMetaRegistry', {
     configurable: true,
-    value: {
-      get: () => null,
-      has: () => false,
-    },
+    value: { get: () => null, has: () => false },
   });
 }
-// ----------------------------------------------------------
+
+jest.mock('@react-native/animated', () => ({}), { virtual: true });
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => ({}), { virtual: true });
