81236b6963cf90935e4f5fb59273c44a79d3a114
europanite <europanite@gmail.com>
2025-09-19 23:44:53 +0900

    fix/testfront

--- frontend/app/jest.setupFiles.js
+++ frontend/app/jest.setupFiles.js
@@ -1,3 +1,14 @@
 jest.mock('@react-native/animated', () => ({}), { virtual: true });
-
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => ({}), { virtual: true });
+
+// --- Disable Expo "winter" import-meta registry in Jest ---
+if (!Object.getOwnPropertyDescriptor(globalThis, '__ExpoImportMetaRegistry')) {
+  Object.defineProperty(globalThis, '__ExpoImportMetaRegistry', {
+    configurable: true,
+    value: {
+      get: () => null,
+      has: () => false,
+    },
+  });
+}
+// ----------------------------------------------------------
